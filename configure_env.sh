f412e33b40058dc84de6000b8afafcc60bcded9b
JustaGist <saifksidhik@gmail.com>
2021-10-13 18:28:27 +0100

    bug fix in env variable setting

--- configure_env.sh
+++ configure_env.sh
@@ -6,14 +6,12 @@ source $HOME/.bashrc
 ROOT_DIR="$(cd $( dirname ${BASH_SOURCE[0]} ) && pwd)"
 cd ${ROOT_DIR}
 
-IRLI_PATH=$(cd ../ && pwd)
-
 
 if [ -z "${IRLI_DIR}" ]
 then
-	  echo "Setting environment variable IRLI_DIR=${IRLI_PATH}"
+	  echo "Setting environment variable IRLI_DIR=${ROOT_DIR}"
 	  echo ' ' >> ${HOME}/.bashrc
-      echo "export IRLI_DIR=${IRLI_PATH}" >> ${HOME}/.bashrc
+      echo "export IRLI_DIR=${ROOT_DIR}" >> ${HOME}/.bashrc
 else
-	  echo "IRLI environment variable already exists and set to IRLI_DIR=${IRLI_PATH}"
+	  echo "IRLI environment variable already exists and set to IRLI_DIR=${ROOT_DIR}"
 fi
